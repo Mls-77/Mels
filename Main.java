503fb4be91c4679373f980cfd91df7e0b3bf8247
Student <melsbekov01@gmail.com>
2026-09-24 11:30:40 +0600

    fkjkdl

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int first = input.nextInt();
+        int second = input.nextInt();
+        System.out.println("sum=" + (first + second));
+    }
+
+}
